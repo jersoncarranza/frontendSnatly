eb312b5b2b351285ac3898fb6cd1e1e5af34973a
Jerson Carranza Lucas <jerson_carranza2@hotmail.com>
2015-03-20 02:36:01 -0500

    snatly 0.1.0 frontend

---
+++ public/js/filesFactory.js
@@ -0,0 +1,19 @@
+angular.module('filesFactory', [])
+       .factory('files', function($http){
+        return {
+          list: function (callback){
+            $http({
+              method: 'GET',
+              url: 'http://127.0.0.1:5000/files/list/',
+              cache: true
+            }).success(callback);
+          },
+          find: function(id, callback){
+            $http({
+              method: 'GET',
+              url: 'http://127.0.0.1:5000/files/see/'+id,
+              cache: true
+            }).success(callback);
+          }
+        };
+      });
